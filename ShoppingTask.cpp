fc8426f0148fac3608fd17a84cdf4f2307e29bf3
garcia-alfredm <garcia.alfredm@gmail.com>
2019-06-30 17:04:14 -0400

    removed source files

--- ShoppingTask.cpp
+++
@@ -1,9 +0,0 @@
-#include "ShoppingTask.h"
-
-ShoppingTask::ShoppingTask(int month, int day, int year, std::string desc, const TaskMetaData& data):
-    Task(month, day, year, desc, data)
-{}
-
-ShoppingTask::ShoppingTask(const Task* task):
-    Task(task)
-{}
